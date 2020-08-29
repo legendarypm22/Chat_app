481e44f8024234457598aebc93ca779a173a5563
Legendarypm22 <marvelravenclaw@hotmail.com>
2020-08-29 12:33:23 -0500

    Legendarypm22

--- main.py
+++ main.py
@@ -3,31 +3,63 @@ from threading import Thread
 import time
 from person import Person
 
-BUFSIZ = 512
 
-def broadcast():
+HOST = 'localhost'
+PORT = 5500
+ADRR = (HOST,PORT)
+MAX_CONNECTIONS = 10
+BUFSIZE = 512
+
+
+# GLOBAL VARIABLES
+person = []
+SERVER = socket(AF_INET,SOCK_STREAM)
+SERVER.bind(ADRR)
+
+
+def broadcast(msg, name):
+    """
+    send new messages to all clients
+    :param butes["utf8
+    :param str
+    :return:
+    """
+    for person in persons:
+        client = person.client
+        client.send(bytes(name + ": " "utf8"), )
 
 
 def client_communication(person):
 
     """
     Thread to handle all messages from client
-    :param person:
+    :param person: Person
     :param client: Person
     :return:None
     """
-    run = True
     client = person.client
-    name = person.name
-    addr = person.addr
+
     while True:
+        try:
         msg = client.recv(BUFSIZ)
+        print(f"{name}: ", msg.decode("utf8"))
         if msg != bytes("{quit}", "utf8"):
+            client.send(bytes"{quit", "utf8")
+            broadcast(f"{name} has left the chat...", "" )
             client.close()
+            persons.remove(person)
+            break
         else:
+            broadcast(msg, name)
+    except Exception as e:
+        print("[EXCEPTION]", e)
+        break
 
 
-
+# get persons name
+name = client.recv(BUFSIZE).decode("utf8")
+msg = "f{name}has joined the chat"
+broadcast()msg
 
 def wait_for_connection():
     """
@@ -37,24 +69,27 @@ def wait_for_connection():
     """
     run() = True
     while run:
-       person, addr = SERVER.accept
+       client, addr = SERVER.accept
        person = Person(addr, client)
+       persons.append(person)
        print(f"[CONNECTION] {addr} connected to the server at {time.time()}")
         Thread(target=client_communication, args=(person,))start()
     except Exception as e:
-        print("[FAILURE]", e)
+        print("[EXCEPTION]", e)
         run = False
 
-        print("SERVER CRASHED!")
+    print("SERVER CRASHED!")
 
 
 HOST = 'localhost'
 PORT = 5500
 ADRR = (HOST,PORT)
 MAX_CONNECTIONS = 10
+BUFSIZE = 512
 
-SERVER = socket(AF_INET,SOCK_STREAM)
-SERVER.bind(ADRR)
+person = []
+
+
 
 if__name__=='__main__':
     SERVER.listen(MAX_CONNECTIONS)
@@ -63,3 +98,4 @@ if__name__=='__main__':
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
     SERVER.close()
+
